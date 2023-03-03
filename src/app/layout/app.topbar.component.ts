877ccb7b88cc497750cc864a50bd26c82a070751
David Emmanuel Pérez García <david220801emmanuel@gmail.com>
2023-03-02 21:40:30 -0600

    Se creo el metodo del logOut

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
 
+import { Router } from '@angular/router';
+
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
@@ -16,5 +18,11 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService,
+                private readonly router: Router) { }
+
+    LogOut():void {
+        localStorage.clear();
+        this.router.navigate(['/'])
+      }
 }
